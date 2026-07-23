3861cc6dec185faa5d1f3158edecefd3b06d2ecd
sungdongyoon <ehdtjd0906@naver.com>
2026-07-24 00:53:12 +0900

    feat : 헤더 레이아웃 작업 완료

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -9,29 +9,37 @@ import { useMobile } from "@/hooks/useMobile";
 const Header = () => {
   const isMobile = useMobile();
 
-  console.log("test", isMobile);
-
   return (
     <header className="w-full h-20 flex bg-background-normal-normal border-b border-line-normal-normal px-20">
       <div className="w-full flex items-center justify-between">
-        <div className="max-w-125 w-full border flex items-center gap-10">
-          <Image alt="로고 이미지" width={100} height={100} src="/next.svg" />
-          <Input placeholder="매장, 주소 검색" aria-label="매장, 주소 검색" />
+        <div className="max-w-125 w-full flex items-center gap-10 flex-1 min-w-0">
+          <Image
+            alt="로고 이미지"
+            width={100}
+            height={100}
+            src="/next.svg"
+            className="shrink-0"
+          />
+          <div className="min-w-0 max-w-125 flex-1">
+            <Input placeholder="매장, 주소 검색" aria-label="매장, 주소 검색" />
+          </div>
         </div>
-        <ul className="flex items-center gap-15 text-label-neutral font-semibold">
-          <li>
-            <Link href="/">지도</Link>
-          </li>
-          <li>
-            <Link href="/places">매장 목록</Link>
-          </li>
-          <li>
-            <Link href="/places/add">매장 추가</Link>
-          </li>
-          <li>
-            <Link href="/">리뷰 목록</Link>
-          </li>
-        </ul>
+        {isMobile ? null : (
+          <ul className="flex items-center gap-15 text-label-neutral font-semibold shrink-0">
+            <li>
+              <Link href="/">지도</Link>
+            </li>
+            <li>
+              <Link href="/places">매장 목록</Link>
+            </li>
+            <li>
+              <Link href="/places/add">매장 추가</Link>
+            </li>
+            <li>
+              <Link href="/">리뷰 목록</Link>
+            </li>
+          </ul>
+        )}
       </div>
     </header>
   );
