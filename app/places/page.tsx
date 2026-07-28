8e4aac5e74153d9deeb9e7354d6e59530acbb5e4
sungdongyoon <ehdtjd0906@naver.com>
2026-07-28 10:09:41 +0900

    feat : 매장 목록 페이지 기본 레이아웃 추가

--- app/places/page.tsx
+++ app/places/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const PlacesPage = () => {
-  return <div className="border">매장 목록 페이지</div>;
+  return (
+    <div className="border">
+      <h1>매장 목록 페이지</h1>
+      <div className="grid grid-cols-12 gap-3">
+        {Array.from({ length: 20 }).map((_, index) => (
+          <div className="col-span-3" key={index}>
+            <div className="bg-black w-full aspect-2/1"></div>
+            <p>원스타 올드패션드 햄버거 도곡점</p>
+            <address>서울특별시 강남구 도곡동 424-11 1층</address>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PlacesPage;
