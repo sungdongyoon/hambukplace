d2d1174b9e061e70453b3974364d09600d4f9385
sungdongyoon <ehdtjd0906@naver.com>
2026-07-30 23:29:27 +0900

    feat : 매장 상세 페이지 임시 사진 섹션 추가

--- components/places/TabPhoto.tsx
+++ components/places/TabPhoto.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const TabPhoto = () => {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-x-5 gap-y-10">
+      {Array.from({ length: 20 }).map((_, index) => (
+        <div
+          className="relative aspect-5/4 w-full overflow-hidden rounded-lg"
+          key={index}
+        >
+          <Image
+            src="/life.jpg"
+            alt="매장 이미지"
+            fill
+            className="object-cover"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TabPhoto;
--- components/places/TabReview.tsx
+++ components/places/TabReview.tsx
@@ -1,5 +1,6 @@
 import { getReviews } from "@/api/reviews";
 import React from "react";
+import { FaStar } from "react-icons/fa6";
 
 const TabReview = async () => {
   // 리뷰 데이터
@@ -10,10 +11,15 @@ const TabReview = async () => {
       {reviewsData.map((review) => (
         <div className="flex items-center gap-5" key={review.id}>
           <div className="bg-neutral-90 aspect-3/2 w-35 shrink-0"></div>
-          <div className="flex flex-col gasp-2">
-            <p>{review.rate}</p>
-            <p>{review.content}</p>
-            <p>{review.visited_at}</p>
+          <div className="flex flex-col gap-2">
+            <p className="flex items-center gap-1 text-[0.7rem] font-semibold">
+              <FaStar className="text-yellow-400" />
+              {review.rate}
+            </p>
+            <p className="text-label-strong text-[0.8rem]">{review.content}</p>
+            <p className="text-label-assistive text-[0.6rem] font-semibold">
+              {review.visited_at} 방문
+            </p>
           </div>
         </div>
       ))}
