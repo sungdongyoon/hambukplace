5896efb14c87b4a13c0f04599cb683299c896da9
sungdongyoon <ehdtjd0906@naver.com>
2026-08-13 09:32:35 +0900

    style : 매장 목록 페이지 썸네일 추가

--- app/places/page.tsx
+++ app/places/page.tsx
@@ -5,17 +5,30 @@ import Link from "next/link";
 import React from "react";
 
 const PlacesPage = async () => {
-  const getPlacesData = await apiGetPlaces();
+  const placesData = await apiGetPlaces();
 
   return (
     <section>
       <PageTitle>매장 목록</PageTitle>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-x-5 gap-y-10">
-        {getPlacesData.map((place) => (
+        {placesData.map((place) => (
           <article key={place.id}>
             <Link href={`/places/${place.id}`}>
-              <div className="w-full relative aspect-3/2 rounded-lg mb-3 border border-line-normal-neutral">
-                {/* <Image alt="매장 이미지" src={place.images[0]} fill /> */}
+              <div className="w-full relative aspect-3/2 rounded-lg mb-3">
+                {place?.images?.length ? (
+                  <Image
+                    alt="매장 이미지"
+                    src={place.images[0]}
+                    fill
+                    className="rounded-lg"
+                  />
+                ) : (
+                  <div className="w-full h-full flex justify-center items-center border border-line-normal-neutral rounded-lg">
+                    <p className="text-[0.6rem] font-medium">
+                      등록된 이미지가 없습니다❗️
+                    </p>
+                  </div>
+                )}
               </div>
               <p className="font-semibold">{place.name}</p>
               <address className="text-[0.7rem] not-italic text-label-alternative">
