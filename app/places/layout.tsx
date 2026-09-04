58a99d30d7f27340b968607ec5efeb9e3466028c
sungdongyoon <ehdtjd0906@naver.com>
2026-09-04 15:58:02 +0900

    style : places layout padding 간격 조정

--- app/places/layout.tsx
+++ app/places/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="w-full max-w-screen-lg py-20 px-5">{children}</div>;
+  return (
+    <div className="w-full max-w-screen-lg py-10 px-5 md:py-20 sm:px-10">
+      {children}
+    </div>
+  );
 };
 
 export default layout;
