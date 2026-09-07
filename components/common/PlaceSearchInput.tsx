361d4ef793ed6c349ffa158448c0134b3ebacfb8
sungdongyoon <ehdtjd0906@naver.com>
2026-09-07 17:13:21 +0900

    refactor : 헤더 매장 검색 input 컴포넌트 페이지별로 기능 분리
    - 메인 페이지(지도)에서 검색할땐 매장 모달 활성화
    - 그 외 다른 페이지에서 검색 시 해당 매장 상세 페이지로 이동

--- components/common/PlaceSearchInput.tsx
+++ components/common/PlaceSearchInput.tsx
@@ -5,15 +5,31 @@ import { usePlaceStore } from "@/store/usePlaceStore";
 import Loading from "./Loading";
 import { usePlacesQuery } from "@/hooks/queries/usePlacesQuery";
 import { twMerge } from "tailwind-merge";
+import { usePathname, useRouter } from "next/navigation";
+import { Place } from "@/types/place";
 
 const PlaceSearchInput = ({ className }: { className?: string }) => {
   const { data: placesData, isLoading } = usePlacesQuery();
   const { placeName, setPlaceName, setSelectedPlaceId } = usePlaceStore();
 
+  const pathname = usePathname();
+  const router = useRouter();
+
   // 검색어 필터
   const filteredPlaces = placeName.trim()
     ? placesData?.filter((el) => el.name.includes(placeName))
     : [];
+
+  // 매장 리스트 클릭 함수
+  const handleClickList = (place: Place) => {
+    if (pathname === "/") {
+      setPlaceName(place.name);
+      setSelectedPlaceId(place.id);
+    } else {
+      router.push(`/places/${place.id}`);
+      setPlaceName("");
+    }
+  };
 
   return (
     <div className={twMerge(`min-w-0 max-w-125 flex-1 relative ${className}`)}>
@@ -39,10 +55,7 @@ const PlaceSearchInput = ({ className }: { className?: string }) => {
                 <li
                   key={el.id}
                   className="border-b border-line-normal-neutral px-4 py-2 font-medium text-[0.8rem] cursor-pointer hover:bg-background-elevated-alternative last:border-0 "
-                  onClick={() => {
-                    setPlaceName(el.name);
-                    setSelectedPlaceId(el.id);
-                  }}
+                  onClick={() => handleClickList(el)}
                 >
                   {el.name}
                 </li>
